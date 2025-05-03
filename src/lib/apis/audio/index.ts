4d1b7be9f4710ff239055f1e461fd9b5be23cc14
OpenHuShen <hushen@alu.sxu.edu.cn>
2025-05-03 18:22:42 +0800

    删除语音transcriptions的Content-Type

--- src/lib/apis/audio/index.ts
+++ src/lib/apis/audio/index.ts
@@ -69,17 +69,18 @@ export const transcribeAudio = async (token: string, file: File) => {
 	data.append('file', file);
 
 	token = 'sk-YQGbgfuNj0YgZdv4zEvoGVcBM4XYnlW3LqEVtf37B4379d0g';
-	data.append('model', 'SenseVoiceSmall');
+	// data.append('model', 'SenseVoiceSmall');
+	data.append('model', 'whisper-1');
 
 	let error = null;
 	// const res = await fetch(`${AUDIO_API_BASE_URL}/transcriptions`, {
-	const res = await fetch(`https://ai.sxu.edu.cn:3300/v1/audio/transcriptions`, {
+	const res = await fetch("https://ai.sxu.edu.cn:3300/v1/audio/transcriptions", {
 		method: 'POST',
 		headers: {
 			// Accept: 'application/json',
 			Accept: '*/*',
 			authorization: `Bearer ${token}`,
-			'Content-Type': 'multipart/form-data'
+			// 'Content-Type': 'multipart/form-data'
 		},
 		body: data
 	})
@@ -114,7 +115,7 @@ export const synthesizeOpenAISpeech = async (
 	let error = null;
 
 	// const res = await fetch(`${AUDIO_API_BASE_URL}/speech`, {
-	const res = await fetch(`https://ai.sxu.edu.cn:3300/v1/audio/speech`, {
+	const res = await fetch("https://ai.sxu.edu.cn:3300/v1/audio/speech", {
 		method: 'POST',
 		headers: {
 			Authorization: `Bearer ${token}`,
